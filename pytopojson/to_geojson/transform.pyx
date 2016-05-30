7e6a25a08ab8709b88166aa53043157b90cf48c2
mthh <mthh@users.noreply.github.com>
2016-05-30 11:27:28 +0200

    Ugly fix to convert topo without transform params

--- pytopojson/to_geojson/transform.pyx
+++ pytopojson/to_geojson/transform.pyx
@@ -3,17 +3,90 @@ ctypedef public struct Point:
     double x
     double y
 
-ctypedef public struct Trans_param:
-    double x
-    double y
+cdef class Transformer_no_transform:
+    cdef list arcs
+    cdef dict dispatch_geom
+    def __init__(self, arcs):
+        self.arcs = arcs
+        self.dispatch_geom = {
+            'Point': self.point, 'MultiPoint': self.multi_point,
+            'LineString': self.line_string, 'MultiLineString': self.multi_line_string_poly,
+            'Polygon': self.multi_line_string_poly, 'MultiPolygon': self.multi_poly,
+            "GeometryCollection": self.geometry_collection
+            }
 
-ctypedef public struct Scale_param:
-    double x
-    double y
+    cdef list stitch_arcs(self, list arcs):
+        cdef list line_string = []
+        for arc in arcs:
+            if arc < 0:
+                line = self.arcs[~arc][::-1]
+            else:
+                line = self.arcs[arc]
+            if len(line_string)>0:
+                if line_string[-1] == line[0]:
+                    line_string.extend(line[1:])
+                else:
+                    line_string.extend(line)
+            else:
+                line_string.extend(line)
+        return line_string
+
+    cdef list stich_multi_arcs(self,arcs):
+        return [self.stitch_arcs(a) for a in arcs]
+
+    cdef feature(self, dict feature):
+        cdef dict out
+        cdef str type_ = feature['type']
+        cdef dict geom_ = {'type': type_}
+        if type_ in ('Point','MultiPoint'):
+            geom_['coordinates'] = feature['coordinates']
+        elif type_ in ('LineString','MultiLineString','MultiPolygon','Polygon'):
+            geom_['arcs'] = feature['arcs']
+        elif type_ == 'GeometryCollection':
+            geom_['geometries'] = feature['geometries']
+
+        geom_ = self.geom_dispatch(geom_)
+        out = {'type':'Feature', 'geometry': geom_}
+        for key in ('properties','bbox','id'):
+            if key in feature:
+                out[key] = feature[key]
+        return out
+
+    def geom_dispatch(self, geometry):
+        _type = geometry['type']
+        return self.dispatch_geom[_type](geometry)
+
+    def point(self, geometry):
+        geometry['coordinates'] = [geometry['coordinates'][0], geometry['coordinates'][1]]
+        return geometry
+
+    def multi_point(self, geometry):
+        geometry['coordinates'] = [[geom[0], geom[1]] for geom in geometry['coordinates']]
+        return  geometry
+
+    def line_string(self,geometry):
+        geometry['coordinates'] = self.stitch_arcs(geometry['arcs'])
+        del geometry['arcs']
+        return geometry
+
+    def multi_line_string_poly(self,geometry):
+        geometry['coordinates'] = self.stich_multi_arcs(geometry['arcs'])
+        del geometry['arcs']
+        return geometry
+
+    def multi_poly(self,geometry):
+        geometry['coordinates'] = [self.stich_multi_arcs(a) for a in geometry['arcs']]
+        del geometry['arcs']
+        return geometry
+
+    def geometry_collection(self, geometry):
+        out = {'type': 'FeatureCollection'}
+        out['features'] = [self.feature(geom) for geom in geometry['geometries']]
+        return out
 
 cdef class Transformer:
-    cdef Scale_param scale
-    cdef Trans_param translate
+    cdef Point scale
+    cdef Point translate
     cdef list arcs
     def __init__(self, dict transform, arcs):
         self.scale.x, self.scale.y = transform['scale']
@@ -51,7 +124,8 @@ cdef class Transformer:
         return [self.stitch_arcs(a) for a in arcs]
 
     cdef list conv_point(self, Point point):
-        return [point.x*self.scale.x+self.translate.x,point.y*self.scale.y+self.translate.y]
+        return [point.x * self.scale.x + self.translate.x,
+                point.y * self.scale.y + self.translate.y]
 
     cpdef convert_point(self, point):
         return self.conv_point({'x': point[0], 'y': point[1]})
@@ -91,14 +165,14 @@ cdef class Transformer:
         geometry['coordinates'] = self.convert_point(geometry['coordinates'])
         return geometry
     def multi_point(self, geometry):
-        geometry['coordinates']= [self.convert_point(geom) for geom in geometry['coordinates']]
+        geometry['coordinates'] = [self.convert_point(geom) for geom in geometry['coordinates']]
         return  geometry
     def line_string(self,geometry):
-        geometry['coordinates']=self.stitch_arcs(geometry['arcs'])
+        geometry['coordinates'] = self.stitch_arcs(geometry['arcs'])
         del geometry['arcs']
         return geometry
     def multi_line_string_poly(self,geometry):
-        geometry['coordinates']=self.stich_multi_arcs(geometry['arcs'])
+        geometry['coordinates'] = self.stich_multi_arcs(geometry['arcs'])
         del geometry['arcs']
         return geometry
     def multi_poly(self,geometry):
